09d3f7c84ed14ef2cd7e0566b3f9b04b0a9dad24
Sam Hawkins <sam.hawkins@vattenfall.com>
2013-07-10 14:56:16 +0200

    Added QueueException

--- customexceptions.py
+++ customexceptions.py
@@ -7,3 +7,8 @@ class ConfigError(Exception):
 class DomainError(Exception):
     """Represents an error trying to access a grid point outside the model domain """
     pass
+    
+class QueueError(Exception):
+    """Represents an queueing system error state for a job """
+    pass
+   
