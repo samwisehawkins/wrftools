ab2bdf56f13ff3503ebfb891b176c47e168385ac
Sam Hawkins <sam.hawkins@vattenfall.com>
2014-12-01 16:47:34 +0100

    updated time-series extraction

--- wrftools/extract.py
+++ wrftools/extract.py
@@ -1,4 +1,31 @@
+"""extract.py provides a thin wrapper around an ncl script to 
+extract WRF variables and diagnostics at specified locations and
+heights above ground level
+
+Usage: 
+    extract.py <file>...
+              [--config=<file>]
+              [--out-dir=<dir>]
+              [--ncl-script=<file>]
+              [--height=<list>]
+              [--loc=<file>]
+              [--opt=<file>]
+              [--mode=<mode>]
+              [--dry-run]
+    
+Options:
+    <file>                  input files to work on
+    --config=<file>         configuration file
+    --out-dir=<dir>         output directory
+    --ncl-script=<file>     location of the ncl script to call, default is ../../extract_time_series.ncl
+    --height=<list>         list of heights to extract to
+    --loc=<file>            file specifying locations to extract to
+    --opt=<file>            ncl file specifying which variables to extract and various ncl options
+    --mode=<mode>           loop over input files, or lump input files together
+    --dry-run               print commands, don't execute"""
+
 import shared
+import confighelper as conf
 from ncdump import ncdump 
 
 import os
@@ -8,47 +35,52 @@ import subprocess
 import time
 
 
-NCL_SCRIPT = 'extract_time_series.ncl'
+NCL_SCRIPT = '%s/../ncl/extract_time_series.ncl' % os.path.split(sys.argv[0])[0]
+SUPPORTED_MODES = ["loop","lump"]
+
+class ConfigError(Exception):
+    pass
 
 
 def main():         
     """ Pass command line arguments to NCL script"""
-    args = docopt.docopt(__doc__, sys.argv[1:])
+    config = conf.config(__doc__, sys.argv[1:])
 
     t0 = time.time()
     
-    if args['--out']==None:    
+    if config['out-dir']==None:    
         out_dir = '.'
     else:
-        out_dir = args['--out']
+        out_dir = config['out-dir']
     
-    
-    if args['--ncl-code']==None:
-        fullpath = os.path.realpath(sys.argv[0])
-        p,f = os.path.split(fullpath)
-        ncl_code_dir = '%s/ncl'% p
+    # if ncl-code-dir not specified, expect it in ../ncl relative to
+    # the path of this file
+    if config['ncl-script']==None:
+        ncl_script = NCL_SCRIPT
     else:
-        ncl_code_dir = args['--ncl-code']
+        ncl_code_dir = config['ncl-script']
 
-    cmd_files = args['<file>']
+    cmd_files = config['<file>']
     # Add nc extension if needed
     nc_files = [ f if f.endswith('.nc') else f+'.nc' for f in cmd_files]
    
     # Create height arrays
-    hgts = args['--height']
-    hgts = '(/%s/)' % ','.join(hgts)
+    hgts = config['height']
+    hgts = '(/%s/)' % ','.join(map(str,hgts))
     
-    mode = args['--mode']
+    mode = config['mode']
     
-    dry_run = args['--dry-run']
+    dry_run = config['dry-run']
     
-    loc = args['--loc']
-    opt = args['--opt']
+    loc = config['loc']
+    opt = config['opt']
     
     
     print '\n*****************************************************'
-    print 'extract_time_series.py' 
-    print args
+    print 'extract.py' 
+    
+    if mode not in SUPPORTED_MODES:
+        raise ConfigError("specified mode not supported")
     
     if mode=='loop':
     
@@ -58,12 +90,12 @@ def main():
             out_file = out_dir+'/'+name.replace('wrfout', 'tseries')
 
             if os.path.exists(out_file):
-                os.rm(out_file)
+                os.remove(out_file)
             
             # Create NCL file array
             in_file = f
             #cmd = """FCST_FILE=%s  NCL_OUT_FILE=%s LOCATIONS_FILE=%s NCL_OPT_FILE=%s ncl %s/%s 'extract_heights=%s' """ %(in_file, out_file, loc,opt, ncl_code_dir, NCL_SCRIPT, hgts)
-            cmd = """NCL_OPT_FILE=%s ncl 'in_file="%s"' 'out_file="%s"' 'extract_heights=%s' 'loc_file="%s"' %s/%s""" % (opt,in_file,out_file, hgts, loc, ncl_code_dir, NCL_SCRIPT)
+            cmd = """NCL_OPT_FILE=%s ncl 'in_file="%s"' 'out_file="%s"' 'extract_heights=%s' 'loc_file="%s"' %s""" % (opt,in_file,out_file, hgts, loc, ncl_script)
             print cmd
             # We could either aggregate all files together or loop over files
             if not dry_run:
@@ -82,7 +114,7 @@ def main():
         # Create NCL file array
         files = '","'.join(sorted(nc_files))
         in_file = '(/"%s"/)' % files
-        cmd = """NCL_OPT_FILE=%s ncl 'in_file=%s' 'out_file="%s"' 'extract_heights=%s' 'loc_file="%s"' %s/%s""" % (opt,in_file,out_file, hgts, loc, ncl_code_dir, NCL_SCRIPT)
+        cmd = """NCL_OPT_FILE=%s ncl 'in_file=%s' 'out_file="%s"' 'extract_heights=%s' 'loc_file="%s"' %s""" % (opt,in_file,out_file, hgts, loc, ncl_script)
         print cmd
         if not dry_run:
             subprocess.call(cmd, shell=True)
@@ -90,8 +122,6 @@ def main():
     
     te = time.time() - t0
     print 'elapsed time: %0.1f ' % te
-if __name__ == '__main__':
-    main()
 
 def extract_tseries(config):
 
@@ -140,3 +170,8 @@ def extract_tseries(config):
         shared.run_cmd(cmd, config)
 
     ncdump(config)
+    
+    
+if __name__ == '__main__':
+    main()
+    
